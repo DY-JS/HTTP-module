2b6eb081e77f56355f7d264c6fd75883f305dfd6
DY-JS <delo1604@ukr.net>
2023-05-14 10:51:46 +0200

    read searchParams

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 import http from 'http';
+import fs from 'fs';
+import url from 'url';
 
 const PORT = process.env.PORT || 8080;
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain'); //Content-Type lkz ntrcnf
 
-  res.end('Hello'); // res.end можно с текстом или без
-  //res.end();
+//req.headers.host === localhost
+//req.url - всё что после localhost:8080
+// Запрос по адресу: 'http://localhost:8080/users/3/friends?sex=m&age=25';
+const server = http.createServer((req, res) => {
+  const normilizedURL = new url.URL(req.url, `http://${req.headers.host}`); //для созд-я объекта адр строки
+  console.log(normilizedURL.searchParams.get('age')); //получили значения пар-ра запроса age - 25
+  console.log(normilizedURL.searchParams.getAll('age')); //массив всех пар-ров age [ '25' ]
+
+  const params = Object.fromEntries(normilizedURL.searchParams.entries()); //получили объект зн-й парам-в
+  console.log(params); //{ sex: 'm', age: '25' }
+  res.end();
 });
+
+//при переходе на http://localhost:8080/index.html
+// const server = http.createServer((req, res) => {
+//   //const fileName = req.url.slice(1) || 'index.html'; // из /index.html делаем index.html
+//   const normilizedURL = new url.URL(req.url, `http://${req.headers.host}`); //для
+//   const fileName = normilizedURL.pathname.slice(1) || 'index.html';
+//   fs.readFile(`./public/${fileName}`, (error, data) => {
+//     //читаем из index.html
+//     if (!error) {
+//       res.end(data);
+//     }
+//     res.statusCode = 404;
+//     res.end();
+//   });
+// });
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   res.setHeader('Content-Type', 'text/plain'); //Content-Type для текста
+
+//   res.end('Hello'); // res.end можно с текстом или без
+//   //res.end();
+// });
 
 server.listen(PORT, () => {
   console.log(`Server statrted on ${PORT}`);
--- request.js
+++ request.js
@@ -1,19 +1,39 @@
 import http from 'http';
+import axios from 'axios';
+//const http = require('http');
 
-const req = http.request('http://localhost:8080', (res) => {
-  console.log(res.statusCode);
-  res.setEncoding('utf8');
-  res.on('data', (data) => {
-    //подписались на событие дата
-    console.log(data);
-    //process.stdout.write(data); //если без res.detEncoding('utf8')
-  });
-});
+const BASE = 'http://localhost:8080';
+const pathname = '/users/3/friends';
+const search = '?sex=m&age=25';
 
-// обработка ошибки
-req.on('error', (error) => {
-  //подписались на событие error
-  console.error(error);
-});
+const href = BASE + pathname + search;
 
-req.end(); //обязательно закрыть запрос
+axios.get(href).catch(() => console.log('Error ocured'));
+
+// axios
+//   .get('http://localhost:8080')
+//   .then((res) => {
+//     console.log(res.status);
+//     console.log(res.data);
+//   })
+//   .catch((error) => {
+//     console.log('error');
+//   });
+
+// const req = http.request('http://localhost:8080', (res) => {
+//   console.log(res.statusCode);
+//   res.setEncoding('utf8');
+//   res.on('data', (data) => {
+//     //подписались на событие дата
+//     console.log(data);
+//     //process.stdout.write(data); //если без res.setEncoding('utf8')
+//   });
+// });
+
+// // обработка ошибки
+// req.on('error', (error) => {
+//   //подписались на событие error
+//   console.error(error);
+// });
+
+// req.end(); //обязательно закрыть запрос
